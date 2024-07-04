18225a9a60981266056f089250f32b8a117dabb2
Rohit Kumar <154122227+rohit-2486@users.noreply.github.com>
2024-07-04 11:43:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express= require('express')
+const app = app.express();
+
+app.use(express.json());
+
+
+app.get("/post", function(req,res){
+
+})
+app.post("/todos", function(req,res){
+
+})
+app.put("/complete", function(req,res){
+    
+})
+
+app.listen(3000);
---
+++ type.js
@@ -0,0 +1,14 @@
+const zod = require("zod");
+
+const createTodo = zod.object({
+    tittle: zod.string(),
+    description: zod.string(),
+})
+const updateTodo = zod.object({
+    id: zod.string(),
+})
+
+module.exports({
+    createTodo:createTodo,
+    updateTodo:updateTodo,
+})
